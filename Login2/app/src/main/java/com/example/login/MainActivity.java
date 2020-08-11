cc9e8db488c31a36fb30c01fcbe8a6e6572a0cc2
divyanshu257 <divyanshu.257@gmail.com>
2020-08-11 14:01:42 +0530

    updated xml

--- Login2/app/src/main/java/com/example/login/MainActivity.java
+++ Login2/app/src/main/java/com/example/login/MainActivity.java
@@ -5,12 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "this is it", Toast.LENGTH_SHORT).show();
+
+        rootNode = FirebaseDatabase.getInstance();
+        reference = rootNode.getReference("users");
+        reference.setValue("some random value");
     }
 }
